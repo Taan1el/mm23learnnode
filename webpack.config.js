1448a44af59e6a9e3ce2f6f52d83e8420fb01489
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2024-09-23 16:40:37 +0300

    Add devserver

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
     plugins: [new HtmlWebpackPlugin()],
 };
