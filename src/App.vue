e5d17a233b8510f1210da677ec7060d922fc7614
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2024-10-07 16:17:33 +0300

    vue and style changes. text box

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import { ref } from 'vue';
  
 let message = ref('Hello Vue!');
-setTimeout(() => {
-    message.value ='Something else'
-}, 10000);
+
 </script>
  
 <template>
-<h1>{{ message }}</h1>
+    <div class="container mt-2">
+        <input class="input" type="text" v-model="message">
+        <div class="content">
+            <h1>{{ message.split('').reverse('').join('') }}</h1>
+        </div>
+    </div>
+    
 </template>
  
