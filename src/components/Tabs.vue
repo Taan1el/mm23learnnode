8c6e654e209e33dbc6bb9e2c00702bdbe94c26f1
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2024-10-16 16:00:46 +0300

    use v tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,16 +1,23 @@
 <script setup>
-import {useRoute, useRouter} from 'vue-router';
+import { RouterLink, useRoute,useRouter } from 'vue-router';
 const route = useRoute();
 const router = useRouter();
-console.log(route, router.getRoutes());
+console.log(route,router.getRoutes());
 
 </script>
+
 <template>
-    <div class="tabs is-centered">
-        <ul>
-            <li v-for="route in $router.getRoutes()" :class="{'is-active': route.path === $route.path}">
-                <RouterLink :to="route.path">{{ route.name }}</RouterLink>
+<div class="tabs is-centered">
+    <ul>
+        <!--<li v-for="route in $router.getRoutes()" :class="{'is-active' : route.path === $route.path}">
+            <RouterLink :to="route.path">{{ route.name }}</RouterLink>
+        </li> -->
+        <RouterLink :to="route.path" v-for="route in $router.getRoutes()"
+            custom v-slot="{isActive, href, navigate}">
+            <li :class="{'is-active':isActive}">
+                <a :href="href" @click="navigate">{{ route.name }}</a>
             </li>
-        </ul>
-    </div>
+        </RouterLink>
+    </ul>
+</div>
 </template>
